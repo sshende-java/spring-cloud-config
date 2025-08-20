64cd371bd5251a7e3086be2d947b66deb1a2f972
sshende-java <80737493+sshende-java@users.noreply.github.com>
2025-08-20 12:00:27 +0530

    Add actuator to config-client to get configs using @RefreshScope

--- config-demo/src/main/java/com/info/config_demo/BuildInfoController.java
+++ config-demo/src/main/java/com/info/config_demo/BuildInfoController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class BuildInfoController {
 
     @Value("${build.name:default}")
